a7936daea7c33441ab6fce2b60edb198f5115381
mohamedwassimzef <mohamedwassim.tlili@esprit.tn>
2026-05-03 17:02:56 +0300

    pre-optimizations

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    AhmedBhs\DoctrineDoctor\DoctrineDoctorBundle::class => ['dev' => true, 'test' => true],
 ];
